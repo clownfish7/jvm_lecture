c98cf4b2e43c2fc242b83b8fa505b09da7165fd9
clownfish7 <279505647@qq.com>
2019-08-02 17:46:43 +0800

    jvm memory

---
+++ src/main/java/com/clownfish7/jvm/memory/Test1.java
@@ -0,0 +1,20 @@
+package com.clownfish7.jvm.memory;
+
+/**
+ * @author yzy
+ * @classname Test1
+ * @description TODO
+ * @create 2019-08-02 17:28
+ */
+
+/**
+ * 虚拟机栈：stack frame 帧栈
+ * 程序计数器(Program Counter)：
+ * 本地方法栈：主要用于处理本地方法
+ * 堆(heap)：JVM管理的最大一块内存空间
+ * 方法区(Method Area)：存储元信息，永久代(Permanent Generation)，从JDK1.8开始，已经彻底废弃永久代，使用元空间(meta space)
+ * 运行时常量池：方法区的一部分内容
+ * 直接内存：Direct Memory
+ */
+public class Test1 {
+}
